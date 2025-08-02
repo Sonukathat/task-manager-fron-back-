33a7b7cf1c78b521d84d2bf57b67ba651c7d5a6b
sonukathat <skkhankathat93@gmail.com>
2025-08-02 14:52:22 +0530

    url update

--- frontend/script.js
+++ frontend/script.js
@@ -75,7 +75,7 @@ const createtask = (taskObj) => {
 // GET tasks from backend
 const getData = async () => {
     try {
-        const res = await fetch(baseUrl);
+        const res = await fetch(`${baseUrl}`);
         const data = await res.json();
         data.map((ele) => createtask(ele));
     } catch (error) {
