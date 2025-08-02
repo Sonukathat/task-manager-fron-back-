e6528eab63e751d27d523003bfa05ce6b0c74739
sonukathat <skkhankathat93@gmail.com>
2025-08-02 14:50:37 +0530

    url update

--- frontend/script.js
+++ frontend/script.js
@@ -75,7 +75,7 @@ const createtask = (taskObj) => {
 // GET tasks from backend
 const getData = async () => {
     try {
-        const res = await fetch('https://task-manager-fron-back.onrender.com');
+        const res = await fetch(baseUrl);
         const data = await res.json();
         data.map((ele) => createtask(ele));
     } catch (error) {
